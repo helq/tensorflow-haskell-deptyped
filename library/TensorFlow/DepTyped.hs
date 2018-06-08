ebdc26ad7b819b07a66d59d0e8be551bbd085ff3
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2018-06-08 16:59:58 +0200

    Adding wrappers to more ops.

--- library/TensorFlow/DepTyped.hs
+++ library/TensorFlow/DepTyped.hs
@@ -17,12 +17,13 @@ module TensorFlow.DepTyped (
   KnownNatList(natListVal),
   constant, placeholder, add, mul, matMul, argMax, softmax, scalar, oneHot,
   oneHot_, reduceMean, softmaxCrossEntropyWithLogits, equal, truncatedNormal,
-  relu, sub, cast, square,
+  relu, sub, cast, square, reshape, shape, sigmoid,
   run, runWithFeeds,
   Tensor, Placeholder, Feed(Feed), FeedList(NilFeedList,(:~~)), render, feed,
   TensorData(TensorData, unTensorData), encodeTensorData,
   Variable(Variable, unVariable), initializedVariable, zeroInitializedVariable, readValue,
   minimizeWith,
+  sigmoidCrossEntropyWithLogits,
 
   Build, Value, Ref, MonadBuild,
   Session, runSession
@@ -30,13 +31,14 @@ module TensorFlow.DepTyped (
 
 import TensorFlow.DepTyped.Ops (constant, placeholder, add, mul, matMul, argMax, softmax, scalar, oneHot,
                                 oneHot_, reduceMean, softmaxCrossEntropyWithLogits, equal, truncatedNormal,
-                                relu, sub, cast, square)
+                                relu, sub, cast, square, reshape, shape, sigmoid)
 import TensorFlow.DepTyped.Session (run, runWithFeeds)
 import TensorFlow.DepTyped.Tensor (Tensor, Placeholder, Feed(Feed), FeedList(NilFeedList,(:~~)), render, feed)
 import TensorFlow.DepTyped.Types (TensorData(TensorData, unTensorData), encodeTensorData)
 import TensorFlow.DepTyped.Base (KnownNatList(natListVal))
 import TensorFlow.DepTyped.Variable (Variable(Variable, unVariable), initializedVariable, zeroInitializedVariable, readValue)
 import TensorFlow.DepTyped.Minimize (minimizeWith)
+import TensorFlow.DepTyped.NN (sigmoidCrossEntropyWithLogits)
 
 import TensorFlow.Core (Build, Value, Ref, MonadBuild)
 import TensorFlow.Session (Session, runSession)
--- library/TensorFlow/DepTyped/Ops.hs
+++ library/TensorFlow/DepTyped/Ops.hs
@@ -40,7 +40,10 @@ module TensorFlow.DepTyped.Ops (
   relu,
   sub,
   cast,
-  square
+  square,
+  reshape,
+  sigmoid,
+  shape
 ) where
 
 import           GHC.TypeLits (Nat, Symbol, KnownNat, natVal)
@@ -56,15 +59,16 @@ import           Data.ByteString (ByteString)
 import qualified TensorFlow.Ops as TF (constant, add, matMul, placeholder, argMax, scalar,
                                        softmax, oneHot, reduceMean, softmaxCrossEntropyWithLogits,
                                        equal, truncatedNormal, vector, mul, relu, sub, cast, abs, sign,
-                                       neg)
-import qualified TensorFlow.GenOps.Core as TF (square)
+                                       neg, reshape, shape)
+import qualified TensorFlow.GenOps.Core as TF (square, sigmoid)
 import qualified TensorFlow.Types as TF (TensorType, Shape(Shape), type OneOf)
 --import qualified TensorFlow.Tensor as TF (Tensor)
 import           TensorFlow.Tensor (Value)
 import           TensorFlow.Build (Build, MonadBuild)
 
 import           TensorFlow.DepTyped.Base (KnownNatList(natListVal), ShapeProduct, UnionPlaceholder,
-                                           BroadcastShapes, RemoveAxisFromShape, AddAxisToEndShape)
+                                           BroadcastShapes, RemoveAxisFromShape, AddAxisToEndShape,
+                                           KnownNatListLength)
 import           TensorFlow.DepTyped.Tensor (Tensor(Tensor), Placeholder)
 
 
@@ -86,14 +90,14 @@ constant :: forall (s :: [Nat]) (n :: Nat) a.
             (TF.TensorType a, ShapeProduct s ~ n, KnownNatList s)
          => Vector n a
          -> Tensor s '[] Build a
-constant v = Tensor $ TF.constant shape (toList v)
-  where shape = TF.Shape . fmap fromInteger $ natListVal (Proxy :: Proxy s)
+constant v = Tensor $ TF.constant shape' (toList v)
+  where shape' = TF.Shape . fmap fromInteger $ natListVal (Proxy :: Proxy s)
 
 placeholder :: forall (name :: Symbol) (shape :: [Nat]) a m.
                (TF.TensorType a, KnownNatList shape, MonadBuild m)
             => m (Placeholder name shape a)
-placeholder = Tensor <$> TF.placeholder shape
-  where shape = TF.Shape . fmap fromInteger $ natListVal (Proxy :: Proxy shape)
+placeholder = Tensor <$> TF.placeholder shape'
+  where shape' = TF.Shape . fmap fromInteger $ natListVal (Proxy :: Proxy shape)
 
 add :: forall (shape1 :: [Nat]) (shape2 :: [Nat]) (phs1 :: [(Symbol, [Nat], Type)]) (phs2 :: [(Symbol, [Nat], Type)]) v1 v2 a.
        TF.OneOf '[(Complex Double), (Complex Float), ByteString, Int16, Int32, Int64, Int8, Word16, Word8, Double, Float] a
@@ -194,3 +198,22 @@ cast (Tensor t) = Tensor $ TF.cast t
 square :: TF.OneOf '[(Complex Double), (Complex Float), Int32, Int64, Word16, Double, Float] a
     => Tensor shape phs v a -> Tensor shape phs Build a
 square (Tensor t) = Tensor (TF.square t)
+
+reshape :: forall t shape1 shape2 phs v.
+           (TF.TensorType t, ShapeProduct shape1 ~ ShapeProduct shape2, KnownNatList shape2)
+        => Tensor shape1 phs v t
+        -> Tensor shape2 phs Build t
+reshape (Tensor t) = Tensor $ TF.reshape t $ TF.vector $ map
+            (fromInteger :: Integer -> Int32)
+            (natListVal (Proxy :: Proxy shape2))
+
+sigmoid :: forall (phs::[(Symbol,[Nat],Type)]) a v s.
+           TF.OneOf '[Complex Double, Complex Float, Word16, Double, Float] a
+        =>   Tensor s phs v a -> Tensor s phs Build a
+sigmoid (Tensor t) = Tensor (TF.sigmoid t)
+
+shape :: forall t shape phs v.
+         (TF.TensorType t, KnownNatList shape) -- , TF.OneOf '[Int32, Int64] out_type
+       => Tensor shape phs v t
+       -> Tensor '[KnownNatListLength shape] phs Build Int32
+shape (Tensor t) = Tensor (TF.shape t)
